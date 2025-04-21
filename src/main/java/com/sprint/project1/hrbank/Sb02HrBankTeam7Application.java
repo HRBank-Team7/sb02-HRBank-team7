bdd3fdd3ebd9b810059bb2f59894349ed86a2a88
sangyeobhan <sangyeobhandev@gmail.com>
2025-04-21 16:18:03 +0900

    initial commit

---
+++ src/main/java/com/sprint/project1/hrbank/Sb02HrBankTeam7Application.java
@@ -0,0 +1,13 @@
+package com.sprint.project1.hrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02HrBankTeam7Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb02HrBankTeam7Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/sprint/project1/hrbank/Sb02HrBankTeam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.project1.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb02HrBankTeam7ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
