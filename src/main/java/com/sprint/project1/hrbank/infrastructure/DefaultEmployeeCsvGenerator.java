8179f957306e93570233f6d388a5a2d08707a27f
쿼카좋아 <a939515@gmail.com>
2025-04-28 09:42:14 +0900

    feat: 엑셀 파일을 리더로 사용시 utf-8 포맷 한글 깨짐 방지

--- src/main/java/com/sprint/project1/hrbank/infrastructure/DefaultEmployeeCsvGenerator.java
+++ src/main/java/com/sprint/project1/hrbank/infrastructure/DefaultEmployeeCsvGenerator.java
@@ -13,12 +13,21 @@ import java.util.stream.Stream;
 @RequiredArgsConstructor
 public class DefaultEmployeeCsvGenerator implements EmployeeCsvGenerator {
 
+  private static final byte[] UTF8_BOM = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
   //TODO: 데이터량 증가 시 스트리밍 파일 작성 검토
   @Override
   public byte[] generateCsvBytes(Stream<Employee> employees) throws IOException {
     StringBuilder sb = new StringBuilder("ID,직원번호,이름,이메일,부서,직급,입사일,상태\n");
     employees.forEach(e -> appendEmployee(sb, e));
-    return sb.toString().getBytes("UTF-8");
+
+    byte[] contentBytes = sb.toString().getBytes("UTF-8");
+
+    // 엑셀 열때 한글 깨짐 방지
+    byte[] result = new byte[UTF8_BOM.length + contentBytes.length];
+    System.arraycopy(UTF8_BOM, 0, result, 0, UTF8_BOM.length);
+    System.arraycopy(contentBytes, 0, result, UTF8_BOM.length, contentBytes.length);
+
+    return result;
   }
 
   private void appendEmployee(StringBuilder sb, Employee e) {
